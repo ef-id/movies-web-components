91b35b69eb1483abaadfd5d60313c74287baaa8f
ef-id <efsun.idriz@gmail.com>
2021-04-11 18:29:45 +0300

    Add router

---
+++ init.js
@@ -0,0 +1,19 @@
+import {Router} from 'https://unpkg.com/@vaadin/router';
+
+const main = document.getElementById('main');
+const router = new Router(main);
+
+// router.setRoutes([
+//     {
+//         path: '/',
+//         component: 'home-component',
+//     },
+//     {
+//         path: '/login',
+//         component: 'login-component',
+//     },
+//     {
+//         path: '/register',
+//         component: 'register-component',
+//     }
+// ])
